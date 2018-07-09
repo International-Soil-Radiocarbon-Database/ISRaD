c9ff653b23796aee30bf4fbf2c0fe4b508ff7014
greymonroe <greymonroe@gmail.com>
2018-07-09 14:24:00 +0200

    update QAQC with new functions

--- R/QAQC.R
+++ R/QAQC.R
@@ -5,6 +5,10 @@
 #' @param file directory to data file
 #' @param writeQCreport if TRUE, a text report of the QC output will be written to the outfile. Default is FALSE
 #' @param outfile filename of the output file if writeQCreport=TRUE. Default is NULL, and the outfile will be written to the directory where the dataset is stored, and named by the dataset being checked.
+#' @import data.tree
+#' @import openxlsx
+#' @import dplyr
+#' @import tidyr
 #' @export
 
 
@@ -227,19 +231,77 @@ for (t in 1:length(names(data))){
     cat("\n\tWARNING: 'lyr_name' mismatch between 'incubation' and 'layer' tabs");error<-error+1
   }
 
-  ##### check values #####
-  cat("\n\nChecking numeric variable columns for non-numeric data...")
+  ##### check numeric values #####
+  cat("\n\nChecking numeric variable columns for innappropriate values...")
+
+  which.nonnum <- function(x) {
+    badNum <- is.na(suppressWarnings(as.numeric(as.character(x))))
+    which(badNum & !is.na(x))
+  }
+
   for (t in 1:length(names(data))){
     tab<-names(data)[t]
+    tab_info<-template_info[[tab]]
     cat("\n",tab,"tab...")
 
     #check for non-numeric values where required
-    numeric_columns<-template_info[[tab]]$Column_Name[template_info[[tab]]$Variable_class=="numeric"]
-    non_numeric_columns<-sapply(numeric_columns, function(c) c %in% colnames(data[[tab]]) & !is.numeric(data[[tab]][[c]]) & !is.logical(data[[tab]][[c]]))
-    if (T %in% non_numeric_columns){
-      cat("\n\tWARNING: non-numeric values detected in these columns:", numeric_columns[non_numeric_columns]);error<-error+1
+    numeric_columns<-tab_info$Column_Name[tab_info$Variable_class=="numeric"]
+    if(length(numeric_columns)<1) next
+    if(tab %in% emptytabs) next
+    for (c in 1:length(numeric_columns)){
+      column<-numeric_columns[c]
+      if(!column %in% colnames(data[[tab]])) next
+      nonnum<-!is.numeric(data[[tab]][,column]) & !is.logical(data[[tab]][,column])
+      if(nonnum) {
+        cat("\n\tWARNING non-numeric values in", column, "column"); error<-error+1
+      } else {
+        max<-as.numeric(tab_info$Max[tab_info$Column_Name == column])
+        min<-as.numeric(tab_info$Min[tab_info$Column_Name == column])
+        toobig<-data[[tab]][,column]>max
+        toosmall<-which(data[[tab]][,column]<min)
+        if(sum(toobig, na.rm=T)>0) {
+          cat("\n\tWARNING values greater than accepted max in", column, "column: rows", toobig); error<-error+1
+        }
+
+        if(sum(toosmall, na.rm=T)>0) {
+          cat("\n\tWARNING values smaller than accepted min in", column, "column: rows", toosmall); error<-error+1
+        }
+
+      }
+
     }
+
   }
+
+  ##### check controlled vocab -----------------------------------------------
+
+
+  cat("\n\nChecking controlled vocab...")
+  for (t in 2:length(names(data))){
+    tab<-names(data)[t]
+    cat("\n",tab,"tab...")
+    tab_info<-template_info[[tab]]
+
+    #check for non-numeric values where required
+    controlled_vocab_columns<-tab_info$Column_Name[tab_info$Variable_class=="character" & !is.na(tab_info$Vocab)]
+
+    for (c in 1:length(controlled_vocab_columns)){
+      column<-controlled_vocab_columns[c]
+      if(!column %in% colnames(data[[tab]])) next
+      controlled_vocab<-tab_info$Vocab[tab_info$Column_Name == column]
+      controlled_vocab<-unlist(strsplit(controlled_vocab, ","))
+      controlled_vocab<-sapply(controlled_vocab, trimws)
+     if(controlled_vocab[1]=="must match across levels") next
+      vocab_check<-sapply(data[[tab]][,column], function(x) x %in% c(controlled_vocab, NA))
+      if(F %in% vocab_check){
+        cat("\n\tWARNING: unacceptable values detected in the", column, "column:", unique(as.character(data[[tab]][,column][!vocab_check]))); error<-error+1
+      }
+
+    }
+
+  }
+
+
 
   ##### Summary #####
 
