8b1c50f66e3db115c97e5bbc86c648644424cf08
greymonroe <greymonroe@gmail.com>
2018-09-06 12:30:33 +0200

    draft compilation scripts for Treat and Yujie added to new _sandbox folder

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ You can install the latest version of ISRaD R package with
 ```
 install_github("International-Soil-Radiocarbon-Database/ISRaD")
 ```
+
+Visit our site for more information:
+https://international-soil-radiocarbon-database.github.io/ISRaD/
---
+++ _sandbox/Sandbox.R
@@ -0,0 +1,16 @@
+library(devtools)
+install("../ISRaD/")
+#install_github("International-Soil-Radiocarbon-Database/ISRaD")
+library(ISRaD)
+
+document()
+
+file="~/Desktop/Data/Angst_unpub.xlsx"
+out<-QAQC(file, writeQCreport = T)
+
+sink(type="message")
+sink()
+
+write.xlsx(ISRaD_database, file = paste0(dataset_directory, "database/ISRaD_list.xlsx"))
+
+compile(dataset_directory = "~/Desktop/Data/", write_report = T, write_out = T)
