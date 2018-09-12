bf63511353e11558db3a63434a63fbe0f0d57802
greymonroe <greymonroe@gmail.com>
2018-09-12 15:31:46 +0200

    more data and compiled

--- _sandbox/Sandbox.R
+++ _sandbox/Sandbox.R
@@ -19,5 +19,5 @@ write.xlsx(ISRaD_database, file = paste0(dataset_directory, "database/ISRaD_list
 compiled<-compile(dataset_directory = "~/Desktop/Data/", write_report = T, write_out = T, return="flat")
 
 compiled<-compile(dataset_directory = "~/Dropbox/USGS/ISRaD_data/Compile_Wed/", write_report = T, write_out = T, return="flat")
+compiled<-compile(dataset_directory = "~/github/ISRaD/Data/", write_report = T, write_out = T, return="flat")
 
-RCurl::url.exists(paste0("https://www.doi.org/","10.1016/j.agrformet.2011.09.015"))
