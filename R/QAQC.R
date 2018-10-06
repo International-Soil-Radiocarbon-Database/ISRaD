d67b366e21e0cd18e9c0c516efeb86b7bd39f9e3
JeffBM <jbeemmil@gmail.com>
2018-10-06 16:12:29 +0200

    Update R/QAQC.R
    Added arg to suppress return of data (usually too much to look at IMO)

--- R/QAQC.R
+++ R/QAQC.R
@@ -5,6 +5,8 @@
 #' @param file directory to data file
 #' @param writeQCreport if TRUE, a text report of the QC output will be written to the outfile. Default is FALSE
 #' @param outfile filename of the output file if writeQCreport=TRUE. Default is NULL, and the outfile will be written to the directory where the dataset is stored, and named by the dataset being checked.
+#' @param summaryStats prints summary statistics. Default is TRUE
+#' @param dataReport prints list structure of database. Default is FALSE
 #' @import data.tree
 #' @import openxlsx
 #' @import dplyr
@@ -13,7 +15,7 @@
 #' @export
 
 
-QAQC <- function(file, writeQCreport=F, outfile=""){
+QAQC <- function(file, writeQCreport=F, outfile="", summaryStats=T, dataReport=F){
 
   ##### setup #####
 
@@ -563,27 +565,26 @@ QAQC <- function(file, writeQCreport=F, outfile=""){
 
 
   # summary statistics ------------------------------------------------------
+  if(summaryStats==T){
+    cat("\n\nIt might be useful to manually review the summary statistics and graphical representation of the data hierarchy as shown below.\n", file=outfile, append = T)
+    cat("\nSummary statistics...\n", file=outfile, append = T)
 
-  cat("\n\nIt might be useful to manually review the summary statistics and graphical representation of the data hierarchy as shown below.\n", file=outfile, append = T)
-  cat("\nSummary statistics...\n", file=outfile, append = T)
+    for (t in 1:length(names(data))){
+      tab<-names(data)[t]
+      data_tab<-data[[tab]]
+      cat("\n",tab,"tab...", file=outfile, append = T)
+      cat(nrow(data_tab), "observations", file=outfile, append = T)
+      if (nrow(data_tab)>0){
+        col_counts<-apply(data_tab, 2, function(x) sum(!is.na(x)))
+        col_counts<-col_counts[col_counts>0]
+        for(c in 1:length(col_counts)){
+          cat("\n   ", names(col_counts[c]),":", col_counts[c], file=outfile, append = T)
 
-  for (t in 1:length(names(data))){
-    tab<-names(data)[t]
-    data_tab<-data[[tab]]
-    cat("\n",tab,"tab...", file=outfile, append = T)
-    cat(nrow(data_tab), "observations", file=outfile, append = T)
-    if (nrow(data_tab)>0){
-      col_counts<-apply(data_tab, 2, function(x) sum(!is.na(x)))
-      col_counts<-col_counts[col_counts>0]
-      for(c in 1:length(col_counts)){
-        cat("\n   ", names(col_counts[c]),":", col_counts[c], file=outfile, append = T)
-
+        }
       }
     }
-  }
 
-
-  cat("\n", rep("-", 20), file=outfile, append = T)
+    cat("\n", rep("-", 20), file=outfile, append = T)
 
 
   # data.tree ---------------------------------------------------------------
@@ -613,16 +614,17 @@ QAQC <- function(file, writeQCreport=F, outfile=""){
   # flat_data$pathString <-  not_na
   # structure <- as.Node(flat_data)
 
-  cat("\n\n", file=outfile, append = T)
+    cat("\n\n", file=outfile, append = T)
   #printed<-print(structure, limit=NULL)
   #sapply(printed$levelName, function(x) cat("\n", x, file=outfile, append = T))
-
+  }
   cat("\n\nPlease email info.israd@gmail.com with concerns or suggestions", file=outfile, append = T)
   cat("\nIf you think there is a error in the functioning of this code please post to
       \nhttps://github.com/International-Soil-Radiocarbon-Database/ISRaD/issues\n", file=outfile, append = T)
 
   attributes(data)$error<-error
 
-  return(data)
-
+  if(dataReport==T){
+    return(data)
+  }
 }
