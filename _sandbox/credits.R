ab04490784d8a2eac2d6618879a3d648d5ae4657
Carlos A. Sierra <csierra@bgc-jena.mpg.de>
2018-10-08 23:23:18 +0200

    Updated references

--- _sandbox/credits.R
+++ _sandbox/credits.R
@@ -1,5 +1,7 @@
 devtools::install_github("International-Soil-Radiocarbon-Database/ISRaD")
 
+setwd("~/Repos/ISRaD/")
+
 library(ISRaD)
 library(rcrossref)
 
@@ -11,7 +13,7 @@ he_doi="10.1126/science.aad4273"
 mathieu_doi="10.1111/gcb.13012"
 
 # References from clean dois
-a=sapply(cleandois[-36],FUN=cr_cn, format="text", style="apa", USE.NAMES = FALSE)
+a=sapply(cleandois[-c(1,67)],FUN=cr_cn, format="text", style="apa", USE.NAMES = FALSE)
 
 he_ref=cr_cn(he_doi,format="text", style="apa")
 mathieu_ref=cr_cn(mathieu_doi,format="text", style="apa")
@@ -35,5 +37,5 @@ p2=paste("Currently, there are", n, "entries in ISRaD, which are from the follow
 # Print markdown file for website
 cat(c(front, f1, f2, f3, f4, f5, front, " ",
              h1, p1, " ", paste("* ",mathieu_ref), paste("* ",he_ref), " ",
-             h2, p2, " ", paste("* ",a)), sep="\n", file="../_pages/credits.md")
+             h2, p2, " ", paste("* ",a)), sep="\n", file="_pages/credits.md")
 
