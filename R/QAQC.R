fb883cbda1e0c83a11686cf010063d67ebfb7d3c
greymonroe <greymonroe@gmail.com>
2018-10-08 12:30:33 -0400

    added checkdoi parameter to QAQC. when set to F, the function doesnt check doi values.

--- R/QAQC.R
+++ R/QAQC.R
@@ -7,6 +7,7 @@
 #' @param outfile filename of the output file if writeQCreport=TRUE. Default is NULL, and the outfile will be written to the directory where the dataset is stored, and named by the dataset being checked.
 #' @param summaryStats prints summary statistics. Default is TRUE
 #' @param dataReport prints list structure of database. Default is FALSE
+#' @param checkdoi set to F if you do not want the QAQC check to validate doi numbers
 #' @import data.tree
 #' @import openxlsx
 #' @import dplyr
@@ -15,7 +16,7 @@
 #' @export
 
 
-QAQC <- function(file, writeQCreport=F, outfile="", summaryStats=T, dataReport=F){
+QAQC <- function(file, writeQCreport=F, outfile="", summaryStats=T, dataReport=F, checkdoi=T){
 
   ##### setup #####
 
@@ -120,6 +121,7 @@ QAQC <- function(file, writeQCreport=F, outfile="", summaryStats=T, dataReport=F
 
 
   ##### check doi --------------------------------------------------------
+  if (checkdoi==T){
   cat("\n\nChecking dataset doi...", file=outfile, append = T)
   dois<-data$metadata$doi
   if(length(dois)<2){if(is.na(dois)) dois<-""}
@@ -127,6 +129,10 @@ QAQC <- function(file, writeQCreport=F, outfile="", summaryStats=T, dataReport=F
     if((!(RCurl::url.exists(paste0("https://www.doi.org/", dois[d])) | dois[d] =="israd"))){
       cat("\n\tWARNING: doi not valid", file=outfile, append = T);error<-error+1
     }
+  }
+  } else {
+    cat("\n\nNot checking dataset doi because 'checkdoi==F'...", file=outfile, append = T)
+    
   }
 
   ##### check for extra or misnamed columns ####
