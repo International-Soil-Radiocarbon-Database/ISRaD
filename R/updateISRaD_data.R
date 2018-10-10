cc3486707914798327b6c783e9cfecce2e1299ba
greymonroe <greymonroe@gmail.com>
2018-10-10 10:05:37 -0600

    comment out update script

--- R/updateISRaD_data.R
+++ R/updateISRaD_data.R
@@ -1,11 +1,10 @@
-
-compiled<-compile(dataset_directory = "~/github/ISRaD/ISRaD_Data/", write_report = T, write_out = T, return="list")
-
-#ISRaD_data<-lapply(compiled[1:8], function(x) x[-c(1,2),])
-ISRaD_data<-lapply(ISRaD_data[], function(x) lapply(x, type.convert))
-
-ISRaD_data[]<-lapply(ISRaD_data, as.data.frame)
-str(ISRaD_data)
-
-#ISRaD_data<-compiled
-devtools::use_data(ISRaD_data, overwrite = T)
+# 
+# compiled<-compile(dataset_directory = "~/github/ISRaD/ISRaD_Data/", write_report = T, write_out = T, return="list")
+# 
+# #ISRaD_data<-lapply(compiled[1:8], function(x) x[-c(1,2),])
+# ISRaD_data<-lapply(ISRaD_data[], function(x) lapply(x, type.convert))
+# 
+# ISRaD_data[]<-lapply(ISRaD_data, as.data.frame)
+# 
+# #ISRaD_data<-compiled
+# devtools::use_data(ISRaD_data, overwrite = T)
