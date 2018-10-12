fe2b810567535728a072a73c368e16f3573e44d3
greymonroe <greymonroe@gmail.com>
2018-10-12 10:30:10 -0600

    added functions for extra

---
+++ R/ISRaD.extra.fill_fm.R
@@ -0,0 +1,15 @@
+#' ISRaD.extra.fill_fm
+#'
+#' @description brief summary description of function
+#' @param database ISRaD dataset object.
+#' @details very detailed description of function, especially if it involves the creation of new columns
+#' @author your name
+#' @references any references of literature or datasets relevant to understand the function. (remove this entire line if there are no references)
+#' @import name of other R package needed to run this function (remove this entire line if there are no required packages)
+#' @export
+
+ISRaD.extra.fill_fm<-function(database){
+  
+  
+  return(database)
+}
---
+++ R/ISRaD.extra.geospatial.soil.R
@@ -0,0 +1,16 @@
+#' ISRaD.extra.geospatial.soil
+#'
+#' @description brief summary description of function
+#' @param database ISRaD dataset object.
+#' @param geodata_directory directory where geospatial climate datasets are found. 
+#' @details very detailed description of function, especially if it involves the creation of new columns
+#' @author your name
+#' @references any references of literature or datasets relevant to understand the function. (remove this entire line if there are no references)
+#' @import name of other R package needed to run this function (remove this entire line if there are no required packages)
+#' @export
+
+ISRaD.extra.geospatial.soil<-function(database, geodata_directory){
+  
+  
+  return(database)
+}
