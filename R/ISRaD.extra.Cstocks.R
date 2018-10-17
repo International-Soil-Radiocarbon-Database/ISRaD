4f25e17588e9fc8be51bba7eb9161abb35e86b77
JeffBM <jbeemmil@gmail.com>
2018-10-17 09:36:54 +0200

    spell check for template info file

--- R/ISRaD.extra.Cstocks.R
+++ R/ISRaD.extra.Cstocks.R
@@ -1,14 +1,13 @@
 #' ISRaD.extra.Cstocks
 #'
-#' @description brief summary description of function
+#' @description Calculates soil organic carbon stock
 #' @param database ISRaD dataset object.
-#' @details very detailed description of function, especially if it involves the creation of new columns
-#' @author your name
+#' @details Function first fills bulk density and organic carbon concentration. SOC stocks can only be calculated if organic carbon concentration and bulk density data are available. SOC stock calculation is for the fine earth fraction (<2mm).
+#' @author J. Beem-Miller
 #' @references any references of literature or datasets relevant to understand the function. (remove this entire line if there are no references)
 #' @export
 
 ISRaD.extra.Cstocks<-function(database){
-  
-  
+
   return(database)
 }
