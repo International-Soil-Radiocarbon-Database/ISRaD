648e8249e4f18c5bbe06514c9dcf3253cacb484d
JeffBM <jbeemmil@gmail.com>
2018-10-17 09:11:16 +0200

    added fill coords to extra fx

--- R/ISRaD.extra.R
+++ R/ISRaD.extra.R
@@ -6,17 +6,18 @@
 #' @export
 
 ISRaD.extra<-function(database=ISRaD_data, geodata_directory){
- 
+
   database<-ISRaD.extra.fill_dates(database)
   database<-ISRaD.extra.fill_14c(database)
+  database<-ISRaD.extra.fill_coords(database)
   database<-ISRaD.extra.delta_delta(database)
   database<-ISRaD.extra.fill_fm(database)
   database<-ISRaD.extra.Cstocks(database)
   database<-ISRaD.extra.geospatial.climate(database, geodata_directory=geodata_directory)
   database<-ISRaD.extra.geospatial.soil(database, geodata_directory=geodata_directory)
-  
+
   ISRaD_data_filled <- database
-  
+
   return(ISRaD_data_filled)
-  
+
 }
