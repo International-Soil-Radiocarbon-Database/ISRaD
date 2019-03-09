84f71a735e4643217448186943472d759fd1eeed
greymonroe <greymonroe@gmail.com>
2019-03-08 20:35:52 -0500

    israd.download now downloads data files from database folder

--- R/ISRaD.download.R
+++ R/ISRaD.download.R
@@ -12,11 +12,11 @@ ISRaD.download<-function(directory = NULL, extra = F){
   }
   
   if (extra){
-    dataURL<-"https://github.com/International-Soil-Radiocarbon-Database/ISRaD/raw/master/data/ISRaD_extra.rda"
-    #dataURL<-"https://github.com/International-Soil-Radiocarbon-Database/ISRaD/raw/master/ISRaD_data_files/database/ISRaD_extra.rda"
+    #dataURL<-"https://github.com/International-Soil-Radiocarbon-Database/ISRaD/raw/master/data/ISRaD_extra.rda"
+    dataURL<-"https://github.com/International-Soil-Radiocarbon-Database/ISRaD/raw/master/ISRaD_data_files/database/ISRaD_extra"
     dataName<-"ISRaD_extra.rda"
     } else {
-    dataURL<-"https://github.com/International-Soil-Radiocarbon-Database/ISRaD/raw/master/ISRaD_data_files/database/ISRaD_data.rda"
+    dataURL<-"https://github.com/International-Soil-Radiocarbon-Database/ISRaD/raw/master/ISRaD_data_files/database/ISRaD_data"
     #dataURL<-"https://github.com/International-Soil-Radiocarbon-Database/ISRaD/raw/master/data/ISRaD_data.rda"
     dataName<-"ISRaD_data.rda"
     
