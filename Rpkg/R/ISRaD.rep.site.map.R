f18db3c0f82f3d0968b6ea5d7ab4c3b40c2381b4
JeffBM <jbeemmil@gmail.com>
2019-11-17 18:46:34 +0100

    removed require call from rep.site.map fx

--- Rpkg/R/ISRaD.rep.site.map.R
+++ Rpkg/R/ISRaD.rep.site.map.R
@@ -14,14 +14,12 @@ ISRaD.rep.site.map<-function(database=NULL){
 
   requireNamespace("ggplot2")
 
-  if(require("maps")) {
-    latlon <- database$site[,3:4]
-    world <- ggplot2::map_data("world")
-    site_map <- ggplot2::ggplot() +
-      geom_polygon(data = world, aes(x=.data$long, y = .data$lat, group = .data$group), fill = NA, color = "Black") +
-      geom_point(data=latlon, aes(x=.data$site_long, y=.data$site_lat), color="red", size=2, alpha=0.5) +
-      theme_bw(base_size=16) +
-      labs(title="ISRaD: Site_Map", x="Longitude", y = "Latitude" )
-      return(site_map)
-    }
+  latlon <- database$site[,3:4]
+  world <- ggplot2::map_data("world")
+  site_map <- ggplot2::ggplot() +
+    geom_polygon(data = world, aes(x=.data$long, y = .data$lat, group = .data$group), fill = NA, color = "Black") +
+    geom_point(data=latlon, aes(x=.data$site_long, y=.data$site_lat), color="red", size=2, alpha=0.5) +
+    theme_bw(base_size=16) +
+    labs(title="ISRaD: Site_Map", x="Longitude", y = "Latitude" )
+  return(site_map)
 }
