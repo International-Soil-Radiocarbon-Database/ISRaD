3f06e9a34e116a8eddb9c493381679d0c77c043f
JeffBM <jbeemmil@gmail.com>
2019-11-18 18:44:21 +0100

    updated Cstocks fx to make new extra vars instead of modifying original vars

--- Rpkg/R/ISRaD.extra.Cstocks.R
+++ Rpkg/R/ISRaD.extra.Cstocks.R
@@ -12,23 +12,29 @@
 #' database.x <- ISRaD.extra.Cstocks(database)
 
 ISRaD.extra.Cstocks<-function(database){
+
+  # make new filled vars
+    database$layer$lyr_coarse_tot_fill_extra <- database$layer[,"lyr_coarse_tot"]
+    database$layer$lyr_bd_samp_fill_extra <- database$layer[,"lyr_bd_samp"]
+    database$layer$lyr_c_inorg_fill_extra <- ifelse(is.na(database$layer$lyr_c_inorg), 0, database$layer$lyr_c_inorg)   # defines missing c_inorg data as c_inorg=0
+    database$layer$lyr_c_org_fill_extra <- database$layer[,"lyr_c_org"]
+    database$layer$lyr_soc_fill_extra <- database$layer[,"lyr_soc"]
+
   # make single BD value from BD sample and BD total
     ix <- which(!is.na(database$layer$lyr_bd_tot) & is.na(database$layer$lyr_coarse_tot))
-    database$layer[ix,"lyr_coarse_tot"] <- 0
+    database$layer[ix,"lyr_coarse_tot_fill_extra"] <- 0 # assumes coarse_tot = 0 if not reported
     ix <- which(is.na(database$layer$lyr_bd_samp) & !is.na(database$layer$lyr_bd_tot))
-    database$layer[ix,"lyr_bd_samp"] <-  database$layer[ix,"lyr_bd_tot"] * (1 - (database$layer[ix,"lyr_coarse_tot"] / 100))
-  # define missing c_inorg data as c_inorg=0
-    database$layer$lyr_c_inorg <- ifelse(is.na(database$layer$lyr_c_inorg), 0, database$layer$lyr_c_inorg)
+    database$layer[ix,"lyr_bd_samp_fill_extra"] <-  database$layer[ix,"lyr_bd_tot"] * (1 - (database$layer[ix,"lyr_coarse_tot_fill_extra"] / 100))
   # replace missing c_org w/ c_tot, accounting for c_inorg
-    ix <- which(is.na(database$layer$lyr_c_org) & database$layer$lyr_c_inorg==0) # id carbonate-free lyrs missing c_org
-    database$layer[ix,"lyr_c_org"] <- database$layer[ix,"lyr_c_tot"] # replace w/ c_tot
-    iix <- which(is.na(database$layer$lyr_c_org) & database$layer$lyr_c_inorg!=0 & !is.na(database$layer$lyr_c_tot)) # id carbonate containing lyrs missing c_org
-    database$layer[iix,"lyr_c_org"] <- database$layer[iix,"lyr_c_tot"]-database$layer[iix,"lyr_c_inorg"] # replace w/ c_tot-c_inorg
+    ix <- which(is.na(database$layer$lyr_c_org) & database$layer$lyr_c_inorg_fill_extra==0) # id carbonate-free lyrs missing c_org
+    database$layer[ix,"lyr_c_org_fill_extra"] <- database$layer[ix,"lyr_c_tot"] # replace w/ c_tot
+    iix <- which(is.na(database$layer$lyr_c_org_fill_extra) & database$layer$lyr_c_inorg_fill_extra!=0 & !is.na(database$layer$lyr_c_tot)) # id carbonate containing lyrs missing c_org
+    database$layer[iix,"lyr_c_org_fill_extra"] <- database$layer[iix,"lyr_c_tot"]-database$layer[iix,"lyr_c_inorg"] # replace w/ c_tot-c_inorg
   # fill OC with SOC stocks if BD data present
-    ix <- which(is.na(database$layer$lyr_c_org) & !is.na(database$layer$lyr_soc) & !is.na(database$layer$lyr_bd_samp) & is.finite(database$layer$lyr_bot) & is.finite(database$layer$lyr_top))
-    database$layer[ix,"lyr_c_org"] <- database$layer[ix,"lyr_soc"]/database$layer[ix,"lyr_bd_samp"]/(database$layer[ix,"lyr_bot"]-database$layer[ix,"lyr_top"])*100
+    ix <- which(is.na(database$layer$lyr_c_org_fill_extra) & !is.na(database$layer$lyr_soc) & !is.na(database$layer$lyr_bd_samp_fill_extra) & is.finite(database$layer$lyr_bot) & is.finite(database$layer$lyr_top))
+    database$layer[ix,"lyr_c_org_fill_extra"] <- database$layer[ix,"lyr_soc"]/database$layer[ix,"lyr_bd_samp_fill_extra"]/(database$layer[ix,"lyr_bot"]-database$layer[ix,"lyr_top"])*100
   # fill lyr_soc if BD and lyr_c_org present
-    ix <- which(is.na(database$layer$lyr_soc) & !is.na(database$layer$lyr_bd_samp) & !is.na(database$layer$lyr_c_org) & is.finite(database$layer$lyr_bot) & is.finite(database$layer$lyr_top))
-    database$layer[ix,"lyr_soc"] <- (database$layer[ix,"lyr_c_org"]/100)*database$layer[ix,"lyr_bd_samp"]*(database$layer[ix,"lyr_bot"]-database$layer[ix,"lyr_top"])
+    ix <- which(is.na(database$layer$lyr_soc) & !is.na(database$layer$lyr_bd_samp_fill_extra) & !is.na(database$layer$lyr_c_org_fill_extra) & is.finite(database$layer$lyr_bot) & is.finite(database$layer$lyr_top))
+    database$layer[ix,"lyr_soc_fill_extra"] <- (database$layer[ix,"lyr_c_org_fill_extra"]/100)*database$layer[ix,"lyr_bd_samp_fill_extra"]*(database$layer[ix,"lyr_bot"]-database$layer[ix,"lyr_top"])
   return(database)
 }
