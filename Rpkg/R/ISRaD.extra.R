edf459b72856d03acf3bf778cb5468b5825c233e
JeffBM <jbeemmil@gmail.com>
2019-12-07 17:59:30 -0800

    new files

--- Rpkg/R/ISRaD.extra.R
+++ Rpkg/R/ISRaD.extra.R
@@ -2,9 +2,7 @@
 #'
 #' @description Fills in transformed and geospatial data where possible, generating an enhanced version of ISRaD.
 #' @param database soilcarbon dataset object
-#' @param geodata_clim_directory directory where geospatial climate data are found
-#' @param geodata_soil_directory directory where geospatial soil data are found
-#' @param geodata_pet_directory directory where geospatial pet data are found
+#' @param geodata_directory directory where geospatial data are found
 #' @details Fills fraction modern, delta 14C, delta-delta values, profile coordinates, BD, orgC, and SOC stocks from entered data; fills soil and climatic data from external geospatial data products
 #' @export
 #' @return returns new ISRaD_extra object with derived, transformed, and filled columns
@@ -14,12 +12,10 @@
 #' database <- ISRaD::Gaudinski_2001
 #' # Fill ISRaD.extra data
 #' database.x <- ISRaD.extra(database,
-#'  geodata_clim_directory = tempdir(),
-#'  geodata_soil_directory = system.file("extdata", "geodata_soil_directory", package = "ISRaD"),
-#'  geodata_pet_directory = system.file("extdata", "geodata_pet_directory", package = "ISRaD"))
+#'  geodata_directory = system.file("extdata", "geodata_directory", package = "ISRaD"))
 #' }
 
-ISRaD.extra<-function(database, geodata_clim_directory, geodata_soil_directory, geodata_pet_directory){
+ISRaD.extra<-function(database, geodata_directory){
 
   message("\t filling dates \n")
   database<-ISRaD.extra.fill_dates(database)
@@ -33,10 +29,8 @@ ISRaD.extra<-function(database, geodata_clim_directory, geodata_soil_directory, 
   database<-ISRaD.extra.delta_delta(database)
   message("\t filling cstocks \n")
   database<-ISRaD.extra.Cstocks(database)
-  message("\t filling geospatial climate data \n")
-  database<-ISRaD.extra.geospatial.climate(database, geodata_clim_directory=geodata_clim_directory, geodata_pet_directory=geodata_pet_directory)
-  message("\t filling 250m spatial soil data  \n")
-  database<-ISRaD.extra.geospatial.soil(database, geodata_soil_directory=geodata_soil_directory)
+  message("\t filling geospatial data \n")
+  database<-ISRaD.extra.geospatial(database, geodata_directory=geodata_directory, fillWorldClim=TRUE)
 
   ISRaD_data_filled <- database
 
