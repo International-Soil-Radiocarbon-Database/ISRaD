7342bb5aeb5a93fc79005005b903bfff09b4c25d
JeffBM <jbeemmil@gmail.com>
2020-02-19 10:36:59 +0100

    fixed warnings issue in ISRaD.rep.count.frc cf. issue #223)

--- Rpkg/R/ISRaD.rep.count.frc.R
+++ Rpkg/R/ISRaD.rep.count.frc.R
@@ -1,6 +1,6 @@
 #' ISRaD.rep.count.frc
 #'
-#' @description Generates a report of fraction level observations, including fraction scheme and properties
+#' @description Generates a report of fraction level observations, including fraction scheme and properties. Note that this function only counts rows, not 14C observations.
 #' @param database ISRaD data object
 #' @import dplyr
 #' @export
@@ -10,11 +10,11 @@
 #' ISRaD.rep.count.frc(database)
 ISRaD.rep.count.frc <- function(database = NULL) {
 
-  frc_data <- suppressWarnings(database$fraction %>% # Start with fraction data
-    left_join(database$layer) %>% # Join to layer data
-    left_join(database$profile) %>% # Join to profile data
-    left_join(database$site) %>% # Join to site data
-    left_join(database$metadata)) # Join to metadata
+  frc_data <- as.data.frame(lapply(database$metadata, as.character), stringsAsFactors = FALSE) %>%
+    right_join(as.data.frame(lapply(database$site, as.character), stringsAsFactors = FALSE), by = "entry_name") %>%
+    right_join(as.data.frame(lapply(database$profile, as.character), stringsAsFactors = FALSE), by = c("entry_name", "site_name")) %>%
+    right_join(as.data.frame(lapply(database$layer, as.character), stringsAsFactors = FALSE), by = c("entry_name", "site_name", "pro_name")) %>%
+    right_join(as.data.frame(lapply(database$fraction, as.character), stringsAsFactors = FALSE), by = c("entry_name", "site_name", "pro_name", "lyr_name"))
   frc_scheme_sum <- count(frc_data, .data$frc_scheme)
   frc_property_sum <- count(frc_data, .data$frc_property)
   return(list(frc_scheme_sum, frc_property_sum))
