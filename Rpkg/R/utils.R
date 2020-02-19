88ec4d0ef166d765f38390ae02f70510682e19f3
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2020-02-19 18:53:07 -0500

    Add is_israd_database function and its tests

--- Rpkg/R/utils.R
+++ Rpkg/R/utils.R
@@ -11,3 +11,30 @@
 lapply_df <- function(...) {
   as.data.frame(lapply(...), stringsAsFactors = FALSE)
 }
+
+
+#' Check whether an object is a valid ISRaD database.
+#'
+#' @param x The object to check
+#' @description A valid ISRaD database is a list with the following elements,
+#' all of which must be \code{\link{data.frame}} objects:
+#' \itemize{
+#' \item{metadata}{Metadata}
+#' \item{site}{Site data}
+#' \item{profile}{Profile data}
+#' \item{flux}{Flux data}
+#' \item{layer}{Layer data}
+#' \item{interstitial}{Interstitial data}
+#' \item{fraction}{Fraction data}
+#' \item{incubation}{Incubation data}
+#' }
+#' @return TRUE or FALSE.
+#' @export
+is_israd_database <- function(x) {
+  # Database is a list and must have all the following data frames
+  tables <- c("metadata", "site", "profile", "flux", "layer", "interstitial",
+              "fraction", "incubation")
+  ok <- is.list(x) &&
+    identical(sort(tables), sort(names(x))) &&
+    all(sapply(x, class) == "data.frame")
+}
--- Rpkg/tests/testthat/test-utils.R
+++ Rpkg/tests/testthat/test-utils.R
@@ -1,10 +1,36 @@
 
 test_that("lapply_df", {
- 
+  
   # Should return a data.frame
   expect_is(lapply_df(cars, length), "data.frame")
   
   # No factors
   x <- lapply_df(cars, function(x) paste(x, collapse = ""))
   expect_identical(class(x$speed), "character")
+})
+
+
+test_that("is_israd_database", {
+  
+  # Handles bad input
+  expect_false(is_israd_database(NULL))
+  
+  # A list but not correct names
+  x <- list()
+  expect_false(is_israd_database(x))
+  
+  # A list with correct names but not data frame
+  x <- list(metadata = data.frame(),
+            site = data.frame(), 
+            profile = data.frame(), 
+            flux = data.frame(), 
+            layer = 1,
+            interstitial = data.frame(),
+            fraction = data.frame(), 
+            incubation = data.frame())
+  expect_false(is_israd_database(x))
+  
+  # A good database
+  x <- ISRaD::Gaudinski_2001
+  expect_true(is_israd_database(x))
 })
