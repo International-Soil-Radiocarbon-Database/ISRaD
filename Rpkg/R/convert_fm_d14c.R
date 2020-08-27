ac1029c25ce381065874c87451cbe82659b55f1f
JeffBM <jbeemmil@gmail.com>
2020-08-27 15:03:35 +0200

    added convenience function for converting fm to d14C and vice versa

---
+++ Rpkg/R/convert_fm_d14c.R
@@ -0,0 +1,24 @@
+#' convert_fm_d14c
+#'
+#' @description convert fraction modern to d14c and d14c to fraction modern
+#' @param fm fraction modern (default NA)
+#' @param d14c delta 14C in per mille (default NA)
+#' @param obs_date_y year of observation/sample collection
+#' @author J. Beem-Miller
+#' @details convenience function for radiocarbon unit conversions
+#' @export
+#' @examples
+#' convert_fm_d14c(fm = 0.97057, obs_date_y = 2005)
+#' convert_fm_d14c(d14c = -35.86611, obs_date_y = 2005)
+
+convert_fm_d14c <- function(fm = NA, d14c = NA, obs_date_y) {
+  lambda <- 0.00012097
+  if(is.na(d14c)) {
+    message("calculating ", "\U0394", "14C from fraction modern")
+    (fm * exp(lambda * (1950 - obs_date_y)) - 1) * 1000
+  } else {
+    message("calculating fraction modern from ", "\U039414C")
+    ((d14c/1000) + 1)/exp(lambda * (1950 - obs_date_y))
+  }
+}
+
