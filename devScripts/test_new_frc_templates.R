2afcfd1b2933b51e4e62013e21e097f32bf6f265
JeffBM <jbeemmil@gmail.com>
2021-12-31 10:43:04 +0100

    updates to new frc template and info files and updated frc templates

--- devScripts/test_new_frc_templates.R
+++ devScripts/test_new_frc_templates.R
@@ -7,17 +7,18 @@ library(dplyr)
 library(RCurl)
 library(httr)
 
-# set wd (assumes ISRaD repo is at root)
-# setwd("./ISRaD/New fraction data/")
-
 # first load new QAQC fx from git repo into your R environment
 # https://github.com/International-Soil-Radiocarbon-Database/ISRaD/blob/master/devScripts/new_frc_QAQC.R
 
+# set wd (assumes ISRaD repo is at root)
+# setwd("./ISRaD/New fraction data/")
+
 # then load the template and template info files (use your local paths)
-template_file <- "./New fraction data/ISRaD_Master_Template_NewFRC.xlsx"
-template_info_file <- "./New fraction data/ISRaD_Template_Info_NewFRC.xlsx"
+template_file <- "./ISRaD_Master_Template_NewFRC.xlsx"
+template_info_file <- "./ISRaD_Template_Info_NewFRC.xlsx"
 
 # Change "file" argument to the path for the file you want to run QAQC on, and go! 
-QAQC_frc(file = "./New fraction data/Fontaine_2007.xlsx",
+QAQC_frc(file = "./Wagai_2015.xlsx",
          template_file = template_file,
-         template_info_file = template_info_file)
+         template_info_file = template_info_file,
+         checkdoi = TRUE)
