28eadd66e682ef8a1f12b398f7097d7a9dc77290
ShaneStoner <sstoner@bgc-jena.mpg.de>
2022-03-18 17:39:28 +0100

    Schrumpf_2013 isotopes and CN extra function
    Missing just a couple of datapoints re: distribution of C between fractions
    
    First commit of C:N ISRaD extra function. Should be quality checked.

---
+++ Rpkg/R/ISRaD.extra.fill_c_to_n.R
@@ -0,0 +1,35 @@
+#' ISRaD.extra.fill_c_to_n
+#'
+#' @description Checks for existing C:N ratios and simply calculates missing values
+#' @param database ISRaD dataset object
+#' @details If missing, C:N ratio is calculated from data provided, avoiding inorganic C if possible
+#' @import From dplyr mutate
+#' @export
+#' @return ISRaD database object with gap-filled C:N ratios in a new column
+#' @examples
+#' \donttest{
+#' # Load example dataset Gaudinski_2001
+#' database <- ISRaD::Gaudinski_2001
+#' # Fill ISRaD.extra data
+#' database.x <- ISRaD.extra.fill_CN(database)
+#' )
+#' }
+ISRaD.extra.fill_CN <- function(database, geodata_directory) {
+  stopifnot(is_israd_database(database))
+
+  database$layer %>%
+    mutate(lyr_fill_c_to_n = ifelse(is.na(lyr_c_to_n), # If yes,
+                               ifelse(is.na(lyr_c_inorg), # then check if total C includes inorganic
+                                      lyr_c_tot / lyr_n_tot, # if not (is.na(c_inorg) == TRUE), use total C and N to calculate C:N
+                                        lyr_c_org / lyr_n_tot), # if inorganic C present (is.na(c_inorg) == FALSE), use organic C only to calculate C:N
+                               lyr_c_to_n)) -> database$layer # if c_to_n != NA, then use the existing value and save
+
+  database$fraction %>%
+    mutate(frc_fill_c_to_n = ifelse(is.na(frc_c_to_n), # If yes,
+                                    ifelse(is.na(frc_c_inorg), # then check if total C includes inorganic
+                                           frc_c_tot / frc_n_tot, # if not (is.na(c_inorg) == TRUE), use total C and N to calculate C:N
+                                           frc_c_org / frc_n_tot), # if inorganic C present (is.na(c_inorg) == FALSE), use organic C only to calculate C:N
+                                    frc_c_to_n)) -> database$fraction # if c_to_n != NA, then use the existing value and save
+
+  return(database)
+}
