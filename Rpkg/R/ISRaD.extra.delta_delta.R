948a9e184b33b1e784cefc50204d32ff737c3373
JeffBM <jbeemmil@gmail.com>
2022-04-13 13:47:48 +0200

    new build

--- Rpkg/R/ISRaD.extra.delta_delta.R
+++ Rpkg/R/ISRaD.extra.delta_delta.R
@@ -1,15 +1,10 @@
 #' ISRaD.extra.delta_delta
 #'
-#' @description Calculates the difference between sample delta 14C and the atmosphere for the year of collection (delta-delta)
+#' @description Calculates the difference between sample delta 14c and the atmosphere for the year of collection (delta-delta)
 #' @param database ISRaD dataset object
-#' @param future Project atmospheric radiocarbon into the future? TRUE/FALSE
-#' @details Creates new column for delta-delta value. Observation year and profile coordinates must be filled (use ISRaD.extra.fill_dates, and ISRaD.extra.fill_coords functions). The relevant atmospheric d14C data (northern or southern hemisphere or tropics) are determined by profile coordinates.
-#' Projection for 2016 to 2021 uses the four quarter average projected atmospheric radiocarbon concentration for Central Europe as estimated in Sierra (2019).
-#' Notes: Central Europe projection used for northern hemisphere as these projections perform better against observations than northern hemisphere projection; southern hemisphere and tropic atmospheric radiocarbon projection lagged by 2.5 per mille, as this is the mean lag observed from 2000 to 2015 in the Graven (2017) dataset.
-#' @author J. Beem-Miller and C. Hicks-Pries
-#' @references Graven et al. 2017 <https://www.geosci-model-dev.net/10/4405/2017/gmd-10-4405-2017.pdf>;  Sierra, C. "Forecasting atmospheric radiocarbon decline to pre-bomb values", Radiocarbon, Vol 60, Nr 4, 2018, p 1055.1066 DOI:10.1017/RDC.2018.33
+#' @details Creates new column for delta-delta value. Observation year and profile coordinates must be filled (use ISRaD.extra.fill_dates, and ISRaD.extra.fill_coords functions). The relevant atmospheric d14c data (northern or southern hemisphere or tropics) are determined by profile coordinates.
+#' @author J. Beem-Miller
 #' @export
-#' @importFrom dplyr left_join
 #' @return ISRaD_data object with new delta delta columns in relevant tables.
 #' @examples
 #' # Load example dataset Gaudinski_2001
@@ -20,23 +15,36 @@
 #' database.x <- ISRaD.extra.fill_dates(database.x)
 #' # Fill delta 14C from fraction modern
 #' database.x <- ISRaD.extra.fill_rc(database.x)
+#' # Fill atmospheric 14c
+#' database.x <- ISRaD.extra.calc_atm14c(database.x)
 #' # Fill delta delta
 #' database.x <- ISRaD.extra.delta_delta(database.x)
 ISRaD.extra.delta_delta <- function(database) {
   stopifnot(is_israd_database(database))
 
-  # run function for flux, layer, interstitial, fraction, and incubation tables
-  c(
-    database[1:3],
-    lapply(seq_along(database[4:8]), function(i) {
+  # get names
+  nms <- names(database)[4:8]
 
-      # get prefixes
-      pre.ls <- c("flx", "lyr", "ist", "frc", "inc")
+  # define prefixes
+  pre <- c("flx", "lyr", "ist", "frc", "inc")
+
+  # run function for flux, layer, interstitial, fraction, and incubation tables
+  ls <- lapply(seq_along(database[4:8]), function(i) {
+
+      # define df
+      df <- database[4:8][[i]]
+
+      # calc dd14c
+      df[[paste0(pre[i], "_dd14c")]] <- df[[paste0(pre[i], "_14c")]] -
+        df[[paste0(pre[i], "_graven_atm")]]
 
       # return
-      database[4:8][i][[paste0(pre.ls[i], "_dd14c")]] <- database[[i]][[paste0(pre.ls[i], "_14c")]] -
-      database[[i]][[paste0(pre.ls[i], "_graven_atm")]]
-      })
-    )
-  )
+      return(df)
+    })
+
+  # rename ls
+  names(ls) <- nms
+
+  # return database
+  c(database[1:3], ls)
 }
