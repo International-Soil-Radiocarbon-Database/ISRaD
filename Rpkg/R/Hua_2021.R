62c0035fc9cd52aceef3856db7ef8aa756aeacaf
JeffBM <jbeemmil@gmail.com>
2023-07-20 22:31:58 +0200

    update Hua object name so does not mask SoilR object

--- Rpkg/R/Hua_2021.R
+++ Rpkg/R/Hua_2021.R
@@ -1,9 +1,10 @@
 #' Hua 2021 dataset for delta-delta calculation
 #'
 #' @description Atmospheric 14C data for the period 1941-2019. 
-#' @details Data from Hua et al., 2021
+#' @details Data from Hua et al., 2021. Smoothed to annual averages.
+#' @usage data(IntCal20)
 #' @references Hua, Q., Turnbull, J., Santos, G., Rakowski, A., Ancapichún, S., De Pol-Holz, R., . . . Turney, C. (2022). ATMOSPHERIC RADIOCARBON FOR THE PERIOD 1950–2019. Radiocarbon, 64(4), 723-745. doi:10.1017/RDC.2021.95
 #'
 #' @format dataframe
 
-"Hua2021"
+"Hua_2021"
