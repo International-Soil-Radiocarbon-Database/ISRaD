8280af2c4db55ca68b0fc3dab51e394db8bc1205
JeffBM <jbeemmil@gmail.com>
2023-07-21 13:55:52 +0200

    fix Hua 2021 script typo

--- Rpkg/R/Hua_2021.R
+++ Rpkg/R/Hua_2021.R
@@ -2,7 +2,7 @@
 #'
 #' @description Atmospheric 14C data for the period 1941-2019. 
 #' @details Data from Hua et al., 2021. Smoothed to annual averages.
-#' @usage data(IntCal20)
+#' @usage data(Hua_2021)
 #' @references Hua, Q., Turnbull, J., Santos, G., Rakowski, A., Ancapichún, S., De Pol-Holz, R., . . . Turney, C. (2022). ATMOSPHERIC RADIOCARBON FOR THE PERIOD 1950–2019. Radiocarbon, 64(4), 723-745. doi:10.1017/RDC.2021.95
 #'
 #' @format dataframe
