4b38c0f5de84471fbd3c7c666bffdaa4e0545c0d
JeffBM <jbeemmil@gmail.com>
2025-08-14 12:40:43 +0200

    fix compile fx

--- Rpkg/R/compile.R
+++ Rpkg/R/compile.R
@@ -123,7 +123,7 @@ compile <- function(dataset_directory,
       # compile template files into list
       soilcarbon_data <- lapply(excel_sheets(data_files[d])[1:8], function(s) data.frame(read_excel(data_files[d], sheet = s)))
       names(soilcarbon_data) <- excel_sheets(data_files[d])[1:8]
-
+      
       # trim description rows
       soilcarbon_data <- lapply(soilcarbon_data, function(x) x <- x[-1:-2, ])
       # trim trailing spaces
@@ -136,30 +136,23 @@ compile <- function(dataset_directory,
         # trim empty rows
         soilcarbon_data[[i]] <- soilcarbon_data[[i]][rowSums(is.na(soilcarbon_data[[i]])) != ncol(soilcarbon_data[[i]]), ]
       }
-
-      # remove excel formating by converting to character
-      soilcarbon_data <- lapply(soilcarbon_data, function(x) lapply(x, as.character))
-
-      # convert back to type values and reduce list back to dataframes
-      soilcarbon_data <- lapply(soilcarbon_data, function(x) lapply(x, utils::type.convert, as.is = FALSE))
-      soilcarbon_data <- lapply(soilcarbon_data, as.data.frame)
-
-      # convert to character again to enable merging with ISRaD_database
+      
+      # remove excel formating and enable merging by converting to character 
       soilcarbon_data <- lapply(soilcarbon_data, function(x) lapply(x, as.character))
       soilcarbon_data <- lapply(soilcarbon_data, as.data.frame)
       
       # Reorder soilcarbon_data to ensure table names are in the same order
       soilcarbon_data <- soilcarbon_data[names(template)]
-
+      
       # merge with template (warnings suppressed b/c variable types will be converted later)
       suppressWarnings(entry <- mapply(bind_rows, template, soilcarbon_data))
       entry <- lapply(entry, function(x) lapply(x, as.character))
       entry <- lapply(entry, as.data.frame, stringsAsFactors = FALSE)
-
+      
       # Compare entry against data in existing database, "ISRaD_old_list"
       diffs <- vector()
       for (i in seq_along(ISRaD_old_list)) {
-
+        
         # Check for entry, verify tables w/ nrow = 0, and ID diffs
         if (unique(entry[["metadata"]]["entry_name"]) %in% names(ISRaD_old_list[[i]])) {
           table <- ISRaD_old_list[[i]][[match(unique(entry[["metadata"]]["entry_name"]), names(ISRaD_old_list[[i]]))]]
@@ -172,12 +165,14 @@ compile <- function(dataset_directory,
           diffs[i] <- ifelse(nrow(entry[[i]]) == 0, 0, 1)
         }
       }
-
+      
       # Run QAQC as needed
       if (sum(diffs) == 0) {
         if (verbose) cat("\n\n", d, "compiling", basename(data_files[d]), "...", "passed QAQC", file = outfile, append = TRUE)
+        
         # merge with ISRaD_database (warnings suppressed b/c variable types will be converted later)
         suppressWarnings(ISRaD_database <- mapply(bind_rows, ISRaD_database, entry))
+        
       } else {
         if (verbose) cat("\n\n", d, "checking", basename(data_files[d]), "...", file = outfile, append = TRUE)
         soilcarbon_data <- QAQC(file = data_files[d], writeQCreport = TRUE, dataReport = TRUE, checkdoi = checkdoi, verbose = TRUE)
@@ -185,14 +180,10 @@ compile <- function(dataset_directory,
           if (verbose) cat("New data - failed QAQC. Check report in QAQC folder.", file = outfile, append = TRUE)
           next
         } else if (verbose) cat("New data - passed QAQC", file = outfile, append = TRUE)
-
-        char_data <- lapply(soilcarbon_data, function(x) x %>% mutate_all(as.character))
-
-        for (t in seq_along(char_data)) {
-          tab <- colnames(char_data)[t]
-          data_tab <- char_data[[t]]
-          ISRaD_database[[t]] <- bind_rows(ISRaD_database[[t]], data_tab)
-        }
+        
+        # merge with ISRaD_database (warnings suppressed b/c variable types will be converted later)
+        suppressWarnings(ISRaD_database <- mapply(bind_rows, ISRaD_database, entry))
+        
       }
     }
   } else {
@@ -202,23 +193,18 @@ compile <- function(dataset_directory,
         setTxtProgressBar(pb, d)
         cat("\n\n", d, "checking", basename(data_files[d]), "...", file = outfile, append = TRUE)
       }
-
+      
       soilcarbon_data <- QAQC(file = data_files[d], writeQCreport = TRUE, dataReport = TRUE, checkdoi = checkdoi, verbose = TRUE)
-
+      
       if (attributes(soilcarbon_data)$error > 0) {
         if (verbose) cat("New template - failed QAQC. Check report in QAQC folder.", file = outfile, append = TRUE)
         next
       } else {
         if (verbose) cat("New template - passed QAQC", file = outfile, append = TRUE)
       }
-
-      char_data <- lapply(soilcarbon_data, function(x) x %>% mutate_all(as.character))
-
-      for (t in seq_along(char_data)) {
-        tab <- colnames(char_data)[t]
-        data_tab <- char_data[[t]]
-        ISRaD_database[[t]] <- bind_rows(ISRaD_database[[t]], data_tab)
-      }
+      
+      # merge with ISRaD_database (warnings suppressed b/c variable types will be converted later)
+      suppressWarnings(ISRaD_database <- mapply(bind_rows, ISRaD_database, entry))
     }
   }
 
